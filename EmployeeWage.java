2e8138079f08c7737a820455ef958a604d0a5395
Supriya Bhurle <supriyabhurle28@gmail.com>
2023-04-11 19:32:58 +0530

    Added : Employee Attendence

--- EmployeeWage.java
+++ EmployeeWage.java
@@ -1,4 +1,12 @@
 public class EmployeeWage {
     public static void main(String[] args) {
         System.out.println("welcome to Employee wage program");
+        int employeeCheck = (int)Math.floor(Math.random()*10)%2;
+        System.out.println(employeeCheck);
+        if (employeeCheck==1){
+            System.out.println("Employee is Present");
+        }
+        else {
+            System.out.println("Employee is absent");
+        }
     }}
